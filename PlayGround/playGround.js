bf4e980aa39423bd83ca625e787e9fef3394570a
williamcastro <william.castro2000@outlook.com>
2022-05-29 11:40:21 -0400

    tweaked the opacity of the manhattan imagine with opacity and added functionality to personal picture

--- PlayGround/playGround.js
+++ PlayGround/playGround.js
@@ -1,6 +1,50 @@
+document.getElementsByClassName('.about')
+.addEventListener('click', function() {
 
+    window.scrollTo ({
+     about: 0,
+     left: 0,
+     behavior: 'smooth'
+    })
+});
 
+    
 
+document.getElementsByClassName('.data');
+data.addEventListener('click', () => {
+    window.scrollTo ({
+        data: 0,
+        left: 0,
+        behavior: 'smooth'
+    })
+});
+
+document.getElementsByClassName('.software');
+software.addEventListner('click', () => {
+    window.scrollTo ({
+        software: 0,
+        left: 0,
+        behavior: 'smooth'
+    })
+});
+
+document.getElementsByClassName('.achievements');
+achievements.addEventListener('click', () => {
+    window.scrollTo ({
+        achievements: 0,
+        left: 0,
+        behavior: 'smooth'
+    })
+});
+
+document.getElementsByClassName('.presentDay');
+presentDay.addEventListener('click', () => {
+    window.scrollTo ({
+        presentDay: 0,
+        left: 0,
+        behavior: 'smooth'
+    })
+});
 
 
 
